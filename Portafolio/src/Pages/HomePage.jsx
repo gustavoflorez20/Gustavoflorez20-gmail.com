2b9dede20a2923aca015c68dc5d4c0cccfac6858
Gustavo <148284090+gustavoflorez20@users.noreply.github.com>
2024-03-17 00:40:54 +0100

    1

---
+++ Portafolio/src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Headers from '../Components/Header';
+import Medium from '../Components/Medium';
+import Footer from '../Components/Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+
+     <Headers/>  
+     <Medium/>
+     <Footer/>
+     
+    </div>
+  )
+}
+
+export default HomePage
